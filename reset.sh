c20fbce89ac129a87caea7b821ceec0f271df2e4
dunvi <dunvi.dunvi@gmail.com>
2013-05-22 00:31:11 -0700

    time distribution stuff

--- reset.sh
+++ reset.sh
@@ -3,4 +3,5 @@ mongo warframe --eval "db.dropDatabase()"
 rm last_id
 touch last_id
 python newscrape.py
+python makeplots.py
 
--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 python newscrape.py
+python makeplots.py
 
