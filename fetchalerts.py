43a8ab292f2118cf2260da9db90126e9bd6f744d
dunvi <dunvi.dunvi@gmail.com>
2013-05-25 13:42:01 -0700

    fixes to offline commits and some fixme stuff :D

--- fetchalerts.py
+++ fetchalerts.py
@@ -39,112 +39,10 @@ api = twitter.Api(consumer_key = credentials.consumer_key(),
 if (api.VerifyCredentials() == None):
     sys.exit("Error with credentials!")
 
+print "Connection made!"
+
 # warframe alert's twitter userid
 warframe = 1344755923
-
-# retrieve the last_id which is in the plaintext file called as such
-#   consider using an evironment variable (pass in to the command line?)
-#   once I have a more stable server setup?
-# if there has been not read (or you wish to repopulate the database),
-#   last_id will be set to an empty string. Check for an empty string
-#   and note that we apparently do not need to parse the id into an int?
-#   or is the python-twitter interface returning an integer here instead
-#   of a string? LOOS LIKE IT IS. FIXME: CHECK THIS
-# otherwise, the last_id is the id of the mot recent tweet retrieved,
-#   for the next time we initialize the API calls
-
-# safe file opening and closing
-with open('last_id') as f:
-    last_id = f.read()
-
-#FIXME: remove the following after checking that the above works
-#f = open('last_id')
-#last_id = f.read()
-#f.close()
-
-# if the database needs to be populated with historical entries
-if last_id == '':
-
-    # current_max_id is the oldest tweet that has been retrieved yet
-    # handles the paging for us, see the twitter API documentation
-    current_max_id = None
-
-    while True:
-        # API call
-        # retrieves 200 tweets at a time - maximum is 3200? FIXME: double check
-        # returns an empty set if there are no more tweets to retrieve
-        alerts = api.GetUserTimeline(user_id = warframe,
-                                     count = 200,
-                                     max_id = current_max_id)
-
-        if alerts == []:
-            break
-    
-        # run through all alerts...
-        for alert in alerts:
-
-            # set the last_id if it's the very first tweet retrieved
-            if last_id == '':
-                with open('last_id', 'w') as f:
-                    f.write(str(alert.id))
-
-                #FIXME: remove the following after checking that the above works
-                #f = open('last_id', 'w')
-                #f.write(str(alert.id))
-                #f.close()
-                
-                last_id = alert.id
-            
-            # max_id is inclusive
-            current_max_id = alert.id - 1
-            current = make_document(alert)
-            
-            # insert the alert into the database!
-            alertdb.insert(current)
-
-# FIXME: I bet the database currently has some entries with the
-# id stored as a string instead of an integer...
-else: 
-    last_id = int(last_id)
-
-# now we have all historical alerts. 
-
-# gather recent updates from the twitter feed
-# first time through, we need to save the since id
-first = True
-current_max_id = None
-while True:
-
-    # apparently this can take in a since_id as a string.
-    #FIXME: double check that this is working as intended?
-    alerts = api.GetUserTimeline(user_id = warframe,
-                                 since_id = last_id,
-                                 count = 200,
-                                 max_id = current_max_id)
-    
-    if alerts == []:
-        break
-
-    for alert in alerts:
-        
-        # save the id of the most recent tweet for next time
-        if first:
-            print "new alerts found!"
-            first = False
-
-            with open('last_id', 'w') as f:
-                f.write(str(alert.id))
-
-            #FIXME: remove the following after checking that the above works
-            #f = open('last_id', 'w')
-            #f.write(str(alert.id))
-            #f.close()
-            
-        current_max_id = alert.id - 1
-
-        current = make_document(alert)
-        
-        alertdb.insert(current)
 
 ###################
 #### FUNCTIONS ####
@@ -227,4 +125,90 @@ def make_document(alert):
 
     return current
 
+# retrieve the last_id which is in the plaintext file called as such
+#   consider using an evironment variable (pass in to the command line?)
+#   once I have a more stable server setup?
+# if there has been not read (or you wish to repopulate the database),
+#   last_id will be set to an empty string. Otherwise parse to int.
+# otherwise, the last_id is the id of the mot recent tweet retrieved,
+#   for the next time we initialize the API calls
+
+# safe file opening and closing
+with open('last_id') as f:
+    last_id = f.read()
+
+# if the database needs to be populated with historical entries
+if last_id == '':
+    print "Populating database"
+
+    # current_max_id is the oldest tweet that has been retrieved yet
+    # handles the paging for us, see the twitter API documentation
+    current_max_id = None
+
+    while True:
+        # API call
+        # retrieves 200 tweets at a time - maximum is 3200
+        # returns an empty set if there are no more tweets to retrieve
+        alerts = api.GetUserTimeline(user_id = warframe,
+                                     count = 200,
+                                     max_id = current_max_id)
+
+        if alerts == []:
+            break
+    
+        # run through all alerts...
+        for alert in alerts:
+
+            # set the last_id if it's the very first tweet retrieved
+            if last_id == '':
+                with open('last_id', 'w') as f:
+                    f.write(str(alert.id))
+                
+                last_id = alert.id
+            
+            # max_id is inclusive
+            current_max_id = alert.id - 1
+            current = make_document(alert)
+            
+            # insert the alert into the database!
+            alertdb.insert(current)
+
+else: 
+    last_id = int(last_id)
+
+# now we have all historical alerts. 
+
+print "Fetching new alerts..."
+
+# gather recent updates from the twitter feed
+# first time through, we need to save the since id
+first = True
+current_max_id = None
+while True:
+
+    alerts = api.GetUserTimeline(user_id = warframe,
+                                 since_id = last_id,
+                                 count = 200,
+                                 max_id = current_max_id)
+    
+    if alerts == []:
+        break
+
+    for alert in alerts:
+        
+        # save the id of the most recent tweet for next time
+        if first:
+            print "New alerts found!"
+            first = False
+
+            with open('last_id', 'w') as f:
+                f.write(str(alert.id))
+       
+        current_max_id = alert.id - 1
+
+        current = make_document(alert)
+        
+        alertdb.insert(current)
+
+print "Done populating database!"
 
